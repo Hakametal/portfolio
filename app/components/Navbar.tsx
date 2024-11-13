a2873b25a93fb1d6cc80f50e413a95ea19ab3c1c
Gerald Regan <hakametal@gmail.com>
2024-11-13 21:28:59 +0100

    fix navbar anchors

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -72,45 +72,45 @@ const Navbar: React.FC = () => {
         <ul>
           {/* Change the order of the li and the anchor tag */}
           <a
-            className="flex justify-between items-center w-full"
+            className="w-40 h-14 flex items-center ml-[-6.25rem] hover:ml-0 duration-300 bg-[#9EA3B0] pr-4 pl-4 border-b-2 border-[#0a192f] rounded-r-lg"
             href="https://www.linkedin.com/in/gerald-regan/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <li className="w-40 h-14 flex items-center ml-[-6.25rem] hover:ml-0 duration-300 bg-[#9EA3B0] pr-4 pl-4 border-b-2 border-[#0a192f] rounded-r-lg">
+            <li className="flex justify-between items-center w-full">
               linkedIn <FaLinkedin size={30} />
             </li>
           </a>
-          <li className="w-40 h-14 flex items-center ml-[-6.25rem] hover:ml-0 duration-300 bg-[#9EA3B0] pr-4 pl-4 border-b-2 border-[#0a192f] rounded-r-lg">
-            <a
-              className="flex justify-between items-center w-full"
-              href="https://github.com/Hakametal"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+          <a
+            className="w-40 h-14 flex items-center ml-[-6.25rem] hover:ml-0 duration-300 bg-[#9EA3B0] pr-4 pl-4 border-b-2 border-[#0a192f] rounded-r-lg"
+            href="https://github.com/Hakametal"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <li className="flex justify-between items-center w-full">
               github <FaGithub size={30} />
-            </a>
-          </li>
-          <li className="w-40 h-14 flex justify-between items-center ml-[-6.25rem] hover:ml-0 duration-300 bg-[#9EA3B0] pr-4 pl-4 border-b-2 border-[#0a192f] rounded-r-lg">
-            <a
-              className="flex justify-between items-center w-full"
-              href="mailto:geraldregan@proton.me"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            </li>
+          </a>
+          <a
+            className="w-40 h-14 flex items-center ml-[-6.25rem] hover:ml-0 duration-300 bg-[#9EA3B0] pr-4 pl-4 border-b-2 border-[#0a192f] rounded-r-lg"
+            href="mailto:geraldregan@proton.me"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <li className="flex justify-between items-center w-full">
               contact <HiOutlineMail size={30} />
-            </a>
-          </li>
-          <li className="w-40 h-14 flex justify-between items-center ml-[-6.25rem] hover:ml-0 duration-300 bg-[#9EA3B0] pr-4 pl-4 border-b-2 border-[#0a192f] rounded-r-lg">
-            <a
-              className="flex justify-between items-center w-full"
-              href="https://drive.google.com/file/d/1KTFJT-x_HDklBVoLjGxkLHnR7IM3RuQC/view?usp=sharing"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            </li>
+          </a>
+          <a
+            className="w-40 h-14 flex items-center ml-[-6.25rem] hover:ml-0 duration-300 bg-[#9EA3B0] pr-4 pl-4 border-b-2 border-[#0a192f] rounded-r-lg"
+            href="https://drive.google.com/file/d/1KTFJT-x_HDklBVoLjGxkLHnR7IM3RuQC/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <li className="flex justify-between items-center w-full">
               resume <BsFillPersonLinesFill size={30} />
-            </a>
-          </li>
+            </li>
+          </a>
         </ul>
       </div>
     </div>
